975b3cdf2c532a4871c61aad54c9fb9d51cb9e4c
Joppe Kroon <j.kroon@topdesk.com>
2022-06-29 12:06:16 +0200

    converted to tabs, using localstorage not sw

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import rimraf from 'rimraf';
 
 import html from '@web/rollup-plugin-html';
-import {terser} from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser';
 import { copy } from '@web/rollup-plugin-copy';
 
 const devMode = (process.env.NODE_ENV === 'development');
@@ -9,32 +9,32 @@ const devMode = (process.env.NODE_ENV === 'development');
 rimraf.sync('docs');
 
 export default {
-  input: './site/*.html',
-  watch: {
-    exclude: 'node_modules/**'
-  },
-  output: {
-    dir: 'docs',
-    format: 'es',
-    assetFileNames: devMode ? "assets/[name]-dev[extname]" : "assets/[name]-[hash][extname]",
-    sourcemap: devMode ? 'inline' : false
-  },
-  plugins: [
-    html({
+	input: './site/*.html',
+	watch: {
+		exclude: 'node_modules/**'
+	},
+	output: {
+		dir: 'docs',
+		format: 'es',
+		assetFileNames: devMode ? 'assets/[name]-dev[extname]' : 'assets/[name]-[hash][extname]',
+		sourcemap: devMode ? 'inline' : false
+	},
+	plugins: [
+		html({
 
-      minify: !devMode
-    }),
-    terser({
-      ecma: 2020,
-      compress: {
-        module: true,
-        drop_console: !devMode,
-        drop_debugger: !devMode
-      }
-    }),
-    copy({
-      rootDir: './site',
-      patterns: ['service-worker.js']
-    })
-  ]
-}
+			minify: !devMode
+		}),
+		terser({
+			ecma: 2020,
+			compress: {
+				module: true,
+				drop_console: !devMode,
+				drop_debugger: !devMode
+			}
+		}),
+		copy({
+			rootDir: './site',
+			patterns: [ 'service-worker.js' ]
+		})
+	]
+};
